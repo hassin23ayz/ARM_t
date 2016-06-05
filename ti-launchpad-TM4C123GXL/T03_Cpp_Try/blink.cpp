ad006bc541ee3eec6d2209caad2bdaed61f6f7c5
ayz03 <hassin23ayz@gmail.com>
2016-06-05 14:43:04 +0600

    cpp tried failed

---
+++ ti-launchpad-TM4C123GXL/T03_Cpp_Try/blink.cpp
@@ -0,0 +1,63 @@
+#include "TM4C123.h"                    // Device header
+#include "RTE_Components.h"             // Component selection
+
+void wait(void);  //using while loop 
+static volatile unsigned int delayTimeTick = 0;
+void my_delay_ms(unsigned int ticksIn_ms);  //using system core clock 
+
+int main(void)
+{
+
+	SystemInit();  //would initialize MCU's clock system, FLASH memoy interface
+	SysTick_Config(SystemCoreClock/1000);
+	//general purpose I/O rin mode Clock gating control register
+	SYSCTL->RCGCGPIO = 0x20;  //enable PORTF only
+	
+	//RGB leds are connected to PF1, PF2, PF3 
+	
+	//GPIO direction register
+	GPIOF->DIR = 0xE;
+	//GPIO digital enable register
+	GPIOF->DEN = 0xE;
+	
+	while(1)
+	{
+		//GPIO Data register
+		
+		GPIOF->DATA = 0x02; //0b0010 red on
+		//wait();
+		my_delay_ms(500);
+		GPIOF->DATA = 0x04; //0b0100 blue on
+		//wait();
+		my_delay_ms(500);
+		GPIOF->DATA = 0x08; //0b1000 green on
+		//wait();
+		my_delay_ms(500);
+	}
+	return 0;
+}
+
+void wait(void)
+{
+	uint32_t clockCounter = 0;
+	while(clockCounter++ < 1000000)
+		;
+}
+
+extern "C" {
+	void SysTick_Handler()
+		{
+			if(delayTimeTick < 0xFFFFFFFF)
+				{
+					delayTimeTick++;
+				}
+		}
+}
+
+
+void my_delay_ms(unsigned int ticksIn_ms)
+{
+	delayTimeTick = 0;
+	while(delayTimeTick < ticksIn_ms)
+	{}
+}
---
+++ ti-launchpad-TM4C123GXL/T03_Cpp_Try/blink.d
@@ -0,0 +1,11 @@
+.\blink.o: blink.cpp
+.\blink.o: C:\Keil_v5\ARM\PACK\Keil\TM4C_DFP\1.0.0\Device\Include\TM4C123\TM4C123.h
+.\blink.o: C:\Keil_v5\ARM\PACK\Keil\TM4C_DFP\1.0.0\Device\Include\TM4C123\TM4C123GH6PM.h
+.\blink.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cm4.h
+.\blink.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\blink.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cmInstr.h
+.\blink.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\cmsis_armcc.h
+.\blink.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cmFunc.h
+.\blink.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cmSimd.h
+.\blink.o: C:\Keil_v5\ARM\PACK\Keil\TM4C_DFP\1.0.0\Device\Include\TM4C123\system_TM4C123.h
+.\blink.o: D:\Git Projects\armt\ti-launchpad-TM4C123GXL\T03_Cpp_Try\RTE\RTE_Components.h
